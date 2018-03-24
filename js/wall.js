920da227b8aa6fcc2b75e8b1fc5f258d9b39bff8
peter <peterzilz@yahoo.de>
2018-03-24 23:11:22 +0100

    Added bounding box to class Wall.

--- js/wall.js
+++ js/wall.js
@@ -12,7 +12,9 @@ class Wall
     constructor(polygon)
     {
         this.polygon = polygon;
-        this.color = "#101010"
+        this.color = "#101010";
+
+        this.calculateBounndingBox();
     }
 
     /**
@@ -28,14 +30,60 @@ class Wall
     render(context, width, height, rangeX, rangeY, offsetX, offsetY)
     {
 
-        let clippedPolygon = this.polygon.map(p => clip(p[0],p[1], width, height, rangeX, rangeY, offsetX, offsetY));
+        if(this.isOutsideScreen(rangeX, rangeY, offsetX, offsetY))
+            return;
 
-        // TODO check, if this polygon will even be visible.
+        let clippedPolygon = this.polygon.map(p => clip(p[0],p[1], width, height, rangeX, rangeY, offsetX, offsetY));
 
         context.beginPath();
         context.fillStyle = this.color;
         clippedPolygon.forEach(p => context.lineTo(p[0],p[1]));
         context.fill()
         context.closePath();
+    }
+
+    /**
+     * Calculates the bounding box of the polygon.
+     */
+    calculateBounndingBox()
+    {
+        let minX = this.polygon[0][0];
+        let minY = this.polygon[0][1];
+        let maxX = this.polygon[0][0];
+        let maxY = this.polygon[0][1];
+
+        this.polygon.forEach(p => {
+            minX = Math.min(p[0], minX);
+            minY = Math.min(p[1], minY);
+            maxX = Math.max(p[0], maxX);
+            maxY = Math.max(p[1], maxY);
+        });
+
+        this.boundingBox = {
+            minX, minY,
+            maxX, maxY
+        };
+    }
+
+    /**
+     * Checks via the bounding box if this element might be visible.
+     * @param {number} rangeX width of the shown screen in coordinates (not pixels)
+     * @param {number} rangeY height of the shown screen in coordinates (not pixels)
+     * @param {number} offsetX coordinates of the center of the shown screen
+     * @param {number} offsetY coordinates of the center of the shown screen
+     * @returns {boolean} true: if the bounding box lies outside the shown scree, false otherwise
+     */
+    isOutsideScreen(rangeX, rangeY, offsetX, offsetY){
+        let top = offsetY + rangeY/2;
+        let bottom = offsetY - rangeY/2;
+        let left = offsetX - rangeX/2;
+        let right = offsetX + rangeX/2;
+
+        return (
+            this.boundingBox.minX > right ||
+            this.boundingBox.maxX < left ||
+            this.boundingBox.minY > top ||
+            this.boundingBox.maxY < bottom
+        );
     }
 }
