68e36c9b9fbf1b41077dbb2280c9ba473fdf4c33
peter <peterzilz@yahoo.de>
2018-03-30 16:43:58 +0200

    Increased performance by rewriting the background renderer (without WebGL).

--- js/background.js
+++ js/background.js
@@ -7,10 +7,10 @@ class Background
 {
     constructor()
     {
-        // this.color1 = "#003f72";
         this.color1 = [0,0x3f,0x72, 255];
-        // this.color2 = "#0069BE";
+        this.color1String = "#003f72";
         this.color2 = [0,0x69,0xbe, 255];
+        this.color2String = "#0069BE";
 
         /** @type {ImageData} */
         this.buffer = null;
--- js/canvas-renderer.js
+++ js/canvas-renderer.js
@@ -6,7 +6,6 @@
 "use strict";
 
 
-
 /**
  * Renders the background on the given context.
  * @param {Background} background the background object to render
@@ -20,47 +19,35 @@
  */
 function renderBackground(background, context, width, height, rangeX, rangeY, offsetX, offsetY)
 {
-    // try to render the background, if possible.
-    // If the view moved, then the buffer cannot be used.
-    if(background.bufferWidth==width && background.bufferHeight==height 
-        && background.bufferOffsetX==offsetX && background.bufferOffsetY==offsetY
-        && background.buffer!=null){
-        context.putImageData(background.buffer, 0, 0);
-        return;
-    }
 
-    background.bufferWidth = width;
-    background.bufferHeight = height;
-    background.bufferOffsetX = offsetX;
-    background.bufferOffsetY = offsetY;
+    let [cornerX, cornerY] = unclip(0, 0,width,height,rangeX,rangeY,offsetX,offsetY);
+    // calculate the length of "1" in terms of pixels
+    let [pixelLengthX, pixelLengthY] = clip(cornerX+1, cornerY-1, width,height,rangeX,rangeY,offsetX,offsetY);
 
-    // TODO rendering each pixel individually is very inefficient!
+    // calculate the field, that has to be filled with background tiles.
+    let minX = Math.floor(cornerX);
+    let maxY = Math.ceil(cornerY);
+    let maxX = Math.ceil(cornerX + rangeX);
+    let minY = Math.floor(cornerY - rangeY);
 
-    let data = new Uint8ClampedArray(4*width*height);
-    let dataCounter = 0;
-    let x,y;
-    let renderColor;
-    for(let j=0;j<height;j++)
+    // fill the background with one color,
+    // so that from the checker pattern only the other color has to be renderd as tiles.
+    context.beginPath();
+    context.fillStyle = background.color2String;
+    context.fillRect(0,0,width,height);
+    context.fill();
+    
+    // render tiles of the other color
+    context.fillStyle = background.color1String;
+    for(let x = minX;x<maxX;x+=2)
     {
-        for(let i=0;i<width;i++)
-        {
-            [x,y] = unclip(i,j,width,height,rangeX,rangeY, offsetX, offsetY);
-            if( (((x%2)+2)%2 < 1) != (((y%2)+2)%2 < 1) )
-                renderColor = background.color1;
-            else 
-                renderColor = background.color2;
-
-            for(let k=0;k<renderColor.length;k++)
-            {
-                data[dataCounter] = renderColor[k];
-                dataCounter++;
-            }
+        for(let y = minY, j=0;y<=maxY;y++,j++){
+            let [px, py] = clip(x+((minX+minY+j)%2),y,width,height,rangeX,rangeY,offsetX,offsetY);
+            context.fillRect(px, py, pixelLengthX, pixelLengthY);
+            context.fill();
         }
     }
-
-    background.buffer = new ImageData(data, width, height);
-
-    context.putImageData(background.buffer, 0, 0);
+    context.closePath();
 }
 
 /**
